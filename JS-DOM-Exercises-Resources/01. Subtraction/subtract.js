e55bb574a5015348ca04929bdd7e20220f1ab278
Georgi Georgiev <63241083+georgi9820@users.noreply.github.com>
2024-06-07 10:12:33 +0300

    Revert "js exercises"
    This reverts commit 9821285216ccb3be87c24aab46938731629e194c.

--- JS-DOM-Exercises-Resources/01. Subtraction/subtract.js
+++
@@ -1,9 +0,0 @@
-function subtract() {
-  let firstNumber = document.getElementById("firstNumber").value;
-  let secondNumber = document.getElementById("secondNumber").value;
-
-  let result = Number(firstNumber) - Number(secondNumber);
-
-  let divElement = document.getElementById("result");
-  divElement.innerText = result;
-}
